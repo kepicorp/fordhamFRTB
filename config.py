4807c7161af17b0d21d51ae40a4295628ae38f9e
Pierre Quemard <pierre.quemard@finastra.com>
2019-08-02 13:04:49 -0400

    Added config parameter "depcrecatedreports" set to 1 to use old back-end

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 # Variable for the project
 
-report = 'testFordham'
+report = 'team0Fordham'
 
 # Other Variable no need to change them
 
-username = 'fo_user'
-password = 'hifo_user'
+username = 'user1'
+password = 'user1'
 jsonresponse = 0
+deprecatedreports = 1
 api = 'http://54.173.12.84:8181/api/'
